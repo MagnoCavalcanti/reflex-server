4fea7579a3d15447149aa5a5e9f8527114508f53
MagnoCavalcanti <magnocavalcanti9@gmail.com>
2025-11-14 15:18:19 -0300

    feat:
    adiciona novas models para o banco de dados

--- app/models/models.py
+++ app/models/models.py
@@ -1,5 +1,5 @@
 from .base import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Text, ForeignKey, Date, CheckConstraint, Boolean
 
 class User(Base):
     __tablename__ = 'users'
@@ -9,3 +9,37 @@ class User(Base):
     email = Column('email', String, nullable=False)
     fullname = Column('fullname', String)
     telephone = Column('telephone', String)
+    
+    __table_args__ = (
+        CheckConstraint("email ~ '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'", name='check_email_format'),
+        CheckConstraint("telephone ~ '^\\(\\d{2}\\)\\s?\\d{5}-\\d{4}$'", name='check_telephone_format'),
+        CheckConstraint("LENGTH(password) >= 8", name='check_password_length'),
+    )
+
+class Course(Base):
+    __tablename__ = 'courses'
+    id = Column('id', Integer, autoincrement=True, primary_key=True)
+    title = Column('title', String, nullable=False, unique=True)
+    description = Column('description', Text)
+    professor_id = Column('professor_id', Integer, ForeignKey('users.id'))
+
+class Module(Base):
+    __tablename__ = 'modules'
+    id = Column('id', Integer, autoincrement=True, primary_key=True)
+    title = Column('title', String, nullable=False)
+    course_id = Column('course_id', Integer, ForeignKey('courses.id'))
+
+class CourseEnrollment(Base):
+    __tablename__ = 'course_enrollments'
+    id = Column('id', Integer, autoincrement=True, primary_key=True)
+    registration_date = Column('registration_date', Date)
+    user_id = Column('user_id', Integer, ForeignKey('users.id'))
+    course_id = Column('course_id', Integer, ForeignKey('courses.id'))
+
+class Lesson(Base):
+    __tablename__ = 'lessons'
+    id = Column('id', Integer, autoincrement=True, primary_key=True)
+    content_type = Column('content_type', String)
+    done = Column('done', Boolean, default=False)
+    module_id = Column('module_id', Integer, ForeignKey('modules.id'))
+    

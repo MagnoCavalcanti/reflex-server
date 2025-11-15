24f8464e29d26b88460e0490559219fff6c1010d
Wpabloz <wesleypablo1311@gmail.com>
2025-11-15 15:54:28 -0300

    feat: altera o main para adicionar novas rotas

--- app/main.py
+++ app/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI, Depends
 
 from .utils import get_current_user
 from .routers import auth_router, course_router
+from app.routers.module_router import router as module_router
+from app.routers.lesson_router import router as lesson_router
+
+
 
 app = FastAPI()
 
@@ -14,3 +18,6 @@ def home():
 
 app.include_router(auth_router)
 app.include_router(course_router)
+app.include_router(module_router)
+app.include_router(lesson_router)
+

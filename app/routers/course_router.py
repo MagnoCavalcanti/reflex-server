c64e9253d39db74c7a19b618914fd8de3cacfd8e
MagnoCavalcanti <magnocavalcanti9@gmail.com>
2026-04-06 21:04:56 -0300

    fix: conserta bug na rota de listar cursos

--- app/routers/course_router.py
+++ app/routers/course_router.py
@@ -10,14 +10,6 @@ from ..schemas import Course as CourseSchema
 
 course_router = APIRouter(prefix="/courses")
 
-@course_router.get("/")
-def list_courses(db: Session = Depends(get_db_session)):
-    course_uc = CoursesUseCases(db)
-    courses = course_uc.list_courses()
-    return JSONResponse(
-        content=jsonable_encoder(courses),
-        status_code=status.HTTP_200_OK
-    )
 
 @course_router.post("/")
 def create_course(course_data: CourseSchema, db: Session = Depends(get_db_session)):
@@ -35,4 +27,13 @@ def enroll_in_course(course_id: int, db: Session = Depends(get_db_session), curr
     return JSONResponse(
         content={ "msg": "success" },
         status_code=status.HTTP_201_CREATED
+    )
+
+@course_router.get("/")
+def list_courses(db: Session = Depends(get_db_session)):
+    course_uc = CoursesUseCases(db)
+    courses = course_uc.list_courses()
+    return JSONResponse(
+        content=jsonable_encoder(courses),
+        status_code=status.HTTP_200_OK
     )
